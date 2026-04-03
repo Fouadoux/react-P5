7be05ff6adec4cb6dcc0713de8c3b517c39ed6bc
benhadda <fouad.benhadda@gmail.com>
2026-04-03 15:18:35 +0200

    fix: accessibility improvements - add main landmark, fix html lang to fr

--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -18,7 +18,7 @@ export const metadata = {
 
 export default function RootLayout({ children }) {
   return (
-    <html lang="en">
+    <html lang="fr">
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased max-w-[1440px] mx-auto`}
       >
